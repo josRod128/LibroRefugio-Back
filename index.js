dcc49168bd5134aaca426bcd41c08bbbdf3cea11
josRod128 <jose12836@gmail.com>
2023-12-06 00:15:58 -0700

    [add] ruta delete y valdacion de cors

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname,"public")));
-
 app.use(cors({
     origin:"*",
-    methods:['GET']
+    methods:['GET','POST','PUT','DELETE']
 }))
 
 app.get('/books', (req, res) => {
     res.json(JSON.parse(fs.readFileSync('./bd.json')));
+});
+
+app.delete('/book/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const books = JSON.parse(fs.readFileSync('./bd.json'));
+    const newBooks = books.filter((book) => book.id !== id);
+    
+    fs.writeFileSync('./bd.json', JSON.stringify(newBooks));
+    res.json(newBooks);
 });
 
 app.listen(port, () => {
