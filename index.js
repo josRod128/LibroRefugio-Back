c8e08f2a02508be081ef1b79439e8021498d9596
josRod128 <jose12836@gmail.com>
2023-12-07 15:31:19 -0700

    [add] trycatch

--- index.js
+++ index.js
@@ -20,74 +20,99 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.get('/books', (req, res) => {
-    res.json(JSON.parse(fs.readFileSync('./bd.json')));
+    try {
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        res.json(books);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 });
 app.get('/books/search/:text', (req, res) => {
-    const data = req.params.text.toLowerCase();
-    const books = JSON.parse(fs.readFileSync('./bd.json'));
-    const book = books.filter((book) => book.title.toLowerCase().includes(data) || book.author.toLowerCase().includes(data));
-
-    res.json(book);
+    try{
+        const data = req.params.text.toLowerCase();
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        const book = books.filter((book) => book.title.toLowerCase().includes(data) || book.author.toLowerCase().includes(data));
+    
+        res.json(book);
+    }catch (error) {
+        res.status(500).json(error.message);
+    }
 });
 app.get('/book/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const books = JSON.parse(fs.readFileSync('./bd.json'));
-    const book = books.filter((book) => book.id === id);
-
-    res.json(book[0]);
+    try{
+        const id = parseInt(req.params.id);
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        const book = books.filter((book) => book.id === id);
+    
+        res.json(book[0]);
+    }catch (error) {
+        res.status(500).json(error.message);
+    }
 });
 
 app.post('/book', (req, res) => {
-    const books = JSON.parse(fs.readFileSync('./bd.json'));
-    const lastBook = books.length > 0 ? books[books.length - 1]['id'] : 0;
-    let date = new Date(req.body.publicationYear).getFullYear();
-    const newBook = {
-        id: lastBook + 1,
-        title: req.body.title,
-        author: req.body.author,
-        yearPublication: date,
-        isbn: req.body.isbn,
-        available: true
-    };
-    books.push(newBook);
-    fs.writeFileSync('./bd.json', JSON.stringify(books));
-    res.send(true);
+    try{
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        const lastBook = books.length > 0 ? books[books.length - 1]['id'] : 0;
+        let date = new Date(req.body.publicationYear).getFullYear();
+        const newBook = {
+            id: lastBook + 1,
+            title: req.body.title,
+            author: req.body.author,
+            yearPublication: date,
+            isbn: req.body.isbn,
+            available: true
+        };
+        books.push(newBook);
+        fs.writeFileSync('./bd.json', JSON.stringify(books));
+        res.send(true);
+    }catch (error) {
+        res.status(500).json(error.message);
+    }
 });
 app.put('/book/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const books = JSON.parse(fs.readFileSync('./bd.json'));
-    const cont = Object.keys(req.body).length;
-    if (cont === 1) {
-        books.forEach((book) => {
-            if (book.id === id) {
-                book.available = req.body.available;
-            }
-        });
-    } else {
-        let date = new Date(req.body.publicationYear).getFullYear();
-        books.forEach((book) => {
-            if (book.id === id) {
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.yearPublication = date;
-                book.isbn = req.body.isbn;
-                book.available = req.body.available;
-            }
-        });
+    try{
+        const id = parseInt(req.params.id);
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        const cont = Object.keys(req.body).length;
+        if (cont === 1) {
+            books.forEach((book) => {
+                if (book.id === id) {
+                    book.available = req.body.available;
+                }
+            });
+        } else {
+            let date = new Date(req.body.publicationYear).getFullYear();
+            books.forEach((book) => {
+                if (book.id === id) {
+                    book.title = req.body.title;
+                    book.author = req.body.author;
+                    book.yearPublication = date;
+                    book.isbn = req.body.isbn;
+                    book.available = req.body.available;
+                }
+            });
+        }
+        fs.writeFileSync('./bd.json', JSON.stringify(books));
+        res.send(true);
+    }catch (error) {
+        res.status(500).json(error.message);
     }
-    fs.writeFileSync('./bd.json', JSON.stringify(books));
-    res.send(true);
 });
 
 
 
 app.delete('/book/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const books = JSON.parse(fs.readFileSync('./bd.json'));
-    const newBooks = books.filter((book) => book.id !== id);
-
-    fs.writeFileSync('./bd.json', JSON.stringify(newBooks));
-    res.json(newBooks);
+    try{
+        const id = parseInt(req.params.id);
+        const books = JSON.parse(fs.readFileSync('./bd.json'));
+        const newBooks = books.filter((book) => book.id !== id);
+    
+        fs.writeFileSync('./bd.json', JSON.stringify(newBooks));
+        res.json(newBooks);
+    }catch (error) {
+        res.status(500).json(error.message);
+    }
 });
 
 
